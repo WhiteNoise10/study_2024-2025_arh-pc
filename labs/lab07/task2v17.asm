88e6bcd6c64bd75fc44191d24135bfac18fa3ede
WhiteNoise10 <dashah2017@gmail.com>
2024-11-23 03:10:39 +0300

    feat(main): add files lab-7

---
+++ labs/lab07/task2v17.asm
@@ -0,0 +1,50 @@
+%include 'in_out.asm'
+section .data
+msg1 db "Введите X: ",0h
+msg2 db "Введите A: ",0h
+msg3 db "Ответ=",0h
+section .bss
+x: RESB 80
+a: RESB 80
+ans: RESB 80
+section .text
+global _start
+_start:
+mov eax,msg1
+call sprint
+mov ecx,x
+mov edx,80
+call sread
+mov eax,x
+call atoi
+mov [x],eax
+mov eax,msg2
+call sprint
+mov ecx,a
+mov edx,80
+call sread
+mov eax,a
+call atoi
+mov [a],eax
+
+mov eax,[a]
+cmp eax,8
+jl lol
+
+mov ebx,[x]
+mul ebx
+jmp ansver
+
+lol:
+mov eax,[a]
+add eax,8
+
+ansver:
+mov [ans],eax
+mov eax,msg3
+call sprint
+mov eax,[ans]
+call iprintLF
+call quit
+
+
